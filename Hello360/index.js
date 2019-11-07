d38a75dd42f522ec4c463d19a4423c2b3b5d4a6b
Saikat Das <saikatdas0790@gmail.com>
2019-11-07 20:14:03 +0530

    Creating a simple counter

--- Hello360/index.js
+++ Hello360/index.js
@@ -1,43 +1,48 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, View, VrButton } from "react-360";
 
 export default class Hello360 extends React.Component {
+  // Our component will keep track of this state
+  state = { count: 0 };
+
+  // This method increments our count, triggering a re-render
+  _incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  componentDidMount() {
+    setInterval(this._incrementCount, 1000);
+  }
+
   render() {
     return (
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
+          <Text style={styles.greeting}>{`Count: ${this.state.count}`}</Text>
         </View>
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    justifyContent: "center",
+    alignItems: "center"
   },
   greetingBox: {
     padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
+    backgroundColor: "#000000",
+    borderColor: "#639dda",
+    borderWidth: 2
   },
   greeting: {
-    fontSize: 30,
-  },
+    fontSize: 30
+  }
 });
 
-AppRegistry.registerComponent('Hello360', () => Hello360);
+AppRegistry.registerComponent("Hello360", () => Hello360);
